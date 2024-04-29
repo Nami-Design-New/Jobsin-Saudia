34301be7c29d8423408cf6acca71f3b4946e3816
Ahmedelsayed265 <ahmedelsayed2102@icloud.com>
2024-04-29 12:44:43 +0300

    updates

--- assets/js/app.js
+++ assets/js/app.js
@@ -6,22 +6,22 @@ let testimonials = new Swiper(".testimonilas", {
   centeredSlides: true,
   pagination: {
     el: ".testimonialsSwiperPagination",
-    clickable: true
+    clickable: true,
   },
   autoplay: {
-    delay: 2500
+    delay: 2500,
   },
   breakpoints: {
     992: {
-      slidesPerView: 3
+      slidesPerView: 3,
     },
     768: {
-      slidesPerView: 2
+      slidesPerView: 2,
     },
     350: {
-      slidesPerView: 1
-    }
-  }
+      slidesPerView: 1,
+    },
+  },
 });
 
 var companies = new Swiper(".companies_grid", {
@@ -31,22 +31,22 @@ var companies = new Swiper(".companies_grid", {
   centeredSlides: true,
   pagination: {
     el: ".testimonialsSwiperPagination",
-    clickable: true
+    clickable: true,
   },
   autoplay: {
-    delay: 2500
+    delay: 2500,
   },
   breakpoints: {
     992: {
-      slidesPerView: 5
+      slidesPerView: 5,
     },
     768: {
-      slidesPerView: 3
+      slidesPerView: 3,
     },
     350: {
-      slidesPerView: 2
-    }
-  }
+      slidesPerView: 2,
+    },
+  },
 });
 var categories = new Swiper(".categories_grid", {
   loop: true,
@@ -55,22 +55,34 @@ var categories = new Swiper(".categories_grid", {
   centeredSlides: true,
   pagination: {
     el: ".testimonialsSwiperPagination",
-    clickable: true
+    clickable: true,
   },
   autoplay: {
-    delay: 2500
+    delay: 2500,
   },
   breakpoints: {
     992: {
-      slidesPerView: 4
+      slidesPerView: 4,
     },
     768: {
-      slidesPerView: 3
+      slidesPerView: 3,
     },
     350: {
-      slidesPerView: 2
-    }
-  }
+      slidesPerView: 2,
+    },
+  },
+});
+var heroSwiper = new Swiper(".heroSection", {
+  loop: true,
+  spaceBetween: 16,
+  speed: 1000,
+  pagination: {
+    el: ".heroSwiperPagination",
+    clickable: true,
+  },
+  autoplay: {
+    delay: 2500,
+  },
 });
 
 $(document).ready(function () {
@@ -127,22 +139,24 @@ function unhighlight(el) {
   }
 }
 
-let searchOpen = document.getElementById("searchOpen");
-let searchContainer = document.querySelector(".search-container");
-let layer = document.querySelector(".layer");
-let closeSearch = document.querySelector(".closeSearch");
-searchOpen.addEventListener("click", () => {
-  searchContainer.classList.add("show");
-  layer.classList.add("show");
-});
-closeSearch.addEventListener("click", () => {
-  searchContainer.classList.remove("show");
-  layer.classList.remove("show");
-});
+// let searchOpen = document.getElementById("searchOpen");
+// let searchContainer = document.querySelector(".search-container");
+// let layer = document.querySelector(".layer");
+// let closeSearch = document.querySelector(".closeSearch");
+// searchOpen.addEventListener("click", () => {
+//   searchContainer.classList.add("show");
+//   layer.classList.add("show");
+// });
+// closeSearch.addEventListener("click", () => {
+//   searchContainer.classList.remove("show");
+//   layer.classList.remove("show");
+// });
 
 // profile menu
 let showBtn = document.getElementById("showProfile");
 let profileMenu = document.querySelector(".profile_menu");
+
+console.log(showBtn , profileMenu);
 
 showBtn.addEventListener("click", () => {
   profileMenu.classList.toggle("show");
@@ -162,4 +176,3 @@ document.addEventListener("click", function (event) {
     profileMenu.classList.remove("show");
   }
 });
-
